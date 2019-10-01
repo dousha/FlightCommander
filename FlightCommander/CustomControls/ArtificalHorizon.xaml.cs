ccefedc497d601b768116ed4e4b5c4282b20ba4b
dousha99 <lijiahao34@gmail.com>
2019-10-01 21:29:52 +0800

    init

---
+++ FlightCommander/CustomControls/ArtificalHorizon.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows.Controls;
+
+namespace FlightCommander.CustomControls
+{
+    /// <summary>
+    /// SpeedIndicator.xaml 的交互逻辑
+    /// </summary>
+    public partial class SpeedIndicator : UserControl
+    {
+        public SpeedIndicator()
+        {
+            InitializeComponent();
+        }
+
+        public float Rotation
+        {
+            set
+            {
+                rotation = value;
+            }
+        }
+
+        public float Roll
+        {
+            set
+            {
+                rotation = value;
+            }
+        }
+
+        public float Yaw
+        {
+            set
+            {
+                yaw = value;
+            }
+        }
+
+        private float yaw = 0.0f;
+        private float rotation = 0.0f;
+    }
+}
